83c641f8b4b611b5e099798d79ee113cb421fb7d
yiday <398504533@qq.com>
2018-02-07 00:06:40 +0800

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-              <h1>Hello React</h1>
+              <h1>Hello React!!</h1>
           </div>
         </div>
       </div>
